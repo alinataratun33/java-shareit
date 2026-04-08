e404f5c02d2c561bc0a0d627f16b3d3baa0652e3
Alina <taratunalina@yandex.by>
2026-04-08 15:01:40 +0300

    Fix

--- src/main/java/ru/practicum/shareit/user/UserService.java
+++ src/main/java/ru/practicum/shareit/user/UserService.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.user;
 import ru.practicum.shareit.user.dto.UserDto;
 
 import java.util.Collection;
-import java.util.Optional;
 
 public interface UserService {
     Collection<UserDto> getAllUser();
